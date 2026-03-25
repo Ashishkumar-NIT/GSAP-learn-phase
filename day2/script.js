7eee3d9d8220cffdc7ff7f508f2c718cc346c494
Ashishkumar-NIT <ashishkumar05164@gmail.com>
2026-03-26 01:40:09 +0530

    scrollTrigger as object

--- day2/script.js
+++ day2/script.js
@@ -1,19 +1,34 @@
+
 gsap.from("#page1 #box",{
     scale:0,
     delay:1,
     duration:2,
-    rotate:360
+    rotate:360,
+    scrollTrigger:{
+        trigger:"#page1 box",
+        scroller:"body",
+        markers:true
+    }
 })
 gsap.from("#page2 #box",{
     scale:0,
-    delay:1,
     duration:2,
-    rotate:360
+    rotate:360,
+    scrollTrigger:{
+        trigger:"#page2 box",
+        scroller:"body",
+        markers:true,
+        start:"top 60%"
+    }
 })
 
 gsap.from("#page3 #box",{
     scale:0,
-    delay:1,
     duration:2,
-    rotate:360
+    rotate:360,
+    scrollTrigger:{
+        trigger:"#page3 box",
+        scroller:"body",
+        markers:true
+    }
 })
