592bee3c209d7a1effbf6423baed3e416a2707d6
Ashishkumar-NIT <ashishkumar05164@gmail.com>
2026-03-26 10:50:07 +0530

    scrub / pin property

--- day2/script.js
+++ day2/script.js
@@ -1,34 +1,94 @@
-
 gsap.from("#page1 #box",{
-    scale:0,
-    delay:1,
+    opacity:0,
     duration:2,
-    rotate:360,
+    // x:500,
+    rotate:720,
+    // delay:1,
     scrollTrigger:{
-        trigger:"#page1 box",
-        scroller:"body",
-        markers:true
-    }
-})
-gsap.from("#page2 #box",{
-    scale:0,
-    duration:2,
-    rotate:360,
-    scrollTrigger:{
-        trigger:"#page2 box",
+        trigger:"#page1 #box",
         scroller:"body",
         markers:true,
-        start:"top 60%"
+        start:"top 30%",
+        end:"top 1%",
+        scrub:3,
+        pin:true
     }
 })
 
-gsap.from("#page3 #box",{
-    scale:0,
-    duration:2,
-    rotate:360,
-    scrollTrigger:{
-        trigger:"#page3 box",
-        scroller:"body",
-        markers:true
-    }
-})
+
+// gsap.from("#page2 h1",{
+//         opacity:0,
+//         scale:0.2,
+//         duration:1,
+//         rotate:480,
+//         x:100,
+//         scrollTrigger:{
+//             trigger:"#page2 h1",
+//             scroller:"body",
+//             markers:true,
+//             start:"top 40%"
+//         }
+
+// })
+
+// gsap.from("#page2 h2",{
+//         opacity:0,
+//         scale:0.2,
+//         duration:1,
+//         rotate:480,
+//         x:100,
+//         scrollTrigger:{
+//             trigger:"#page2 h2",
+//             scroller:"body",
+//             markers:true,
+//             start:"top 40%"
+//         }
+
+// })
+// gsap.from("#page3 #box",{
+//     opacity:0,
+//     duration:2,
+//     x:500,
+//     // delay:1,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 100%"
+//     }
+// })
+
+
+// gsap.from("#page1 #box",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360,
+//     scrollTrigger:{
+//         trigger:"#page1 box",
+//         scroller:"body",
+//         markers:true
+//     }
+// })
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     duration:2,
+//     rotate:360,
+//     scrollTrigger:{
+//         trigger:"#page2 box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%"
+//     }
+// })
+
+// gsap.from("#page3 #box",{
+//     scale:0,
+//     duration:2,
+//     rotate:360,
+//     scrollTrigger:{
+//         trigger:"#page3 box",
+//         scroller:"body",
+//         markers:true
+//     }
+// })
